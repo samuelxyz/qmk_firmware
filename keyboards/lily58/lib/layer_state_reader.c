2309d5fdca246eb513ed373eafa897a30cd39208
Samuel Tan <xinyizhentan@gmail.com>
2022-10-30 21:46:38 -0400

    initial tanamr keymap

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -4,10 +4,11 @@
 #include "lily58.h"
 
 #define L_BASE 0
-#define L_LOWER (1 << 1)
-#define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
+#define L_QWER (1 << 1)
+#define L_GAME (1 << 2)
+#define L_GAMEFN (1 << 3)
+#define L_SYM (1 << 4)
+#define L_EXTEND (1 << 5)
 
 char layer_state_str[24];
 
@@ -15,20 +16,25 @@ const char *read_layer_state(void) {
   switch (layer_state)
   {
   case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Rollla rollla rollla!");
     break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+  case L_GAME:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Game");
     break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+  case L_QWER:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: QWERTY");
     break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+  case L_GAMEFN:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Game Fn");
+    break;
+  case L_SYM:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Sym/Numpad");
+    break;
+  case L_EXTEND:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Extend");
     break;
   default:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: %u", layer_state);
   }
 
   return layer_state_str;
