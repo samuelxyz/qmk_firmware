73f91ee324cff973aa57ea423cd6c2cf81b39a9f
Samuel Tan <xinyizhentan@gmail.com>
2022-11-13 23:07:35 -0500

    gaming layer tweaks

--- keyboards/lily58/keymaps/tanamr/keymap.c
+++ keyboards/lily58/keymaps/tanamr/keymap.c
@@ -69,25 +69,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* GAME
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |      |      |      |      |      |      |
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |      |      |  Up  |      |      |      |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |  Up  |   O  |   P  |  '   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |GameFn|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|      | Left |  Dn  |Right |      |      |
- * |------+------+------+------+------+------|   H   |    |       |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|      |      |      |      |      |      |
+ * |GameFn|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  | Left |  Dn  |Right |   ;  | Bksp |
+ * |------+------+------+------+------+------|   H   |    |  F2   |------+------+------+------+------+------|
+ * |LCtrl |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  -   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt |RShift|Space | / Enter /       \      \  |      |      |      |
+ *                   | LAlt | Shift|Space | / Enter /       \   I  \  |   J  |   K  |   L  |
  *                   |(RCtrl)      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
 [_GAME] = LAYOUT(
-  KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-  KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO,
-  MO(_GAMEFN), KC_A, KC_S, KC_D, KC_F, KC_G, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,
-  KC_NO, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_H, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-  KC_RCTL, KC_RSFT, KC_SPC, KC_ENT, KC_NO, KC_NO, KC_NO, KC_NO
+  KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
+  KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y,    KC_U,    KC_UP,    KC_O,    KC_P,    KC_QUOT,
+  MO(_GAMEFN), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_LEFT, KC_DOWN, KC_RGHT, KC_QUOT,
+  KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_H, KC_F2, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+  KC_RCTL, KC_LSFT, KC_SPC, KC_ENT, KC_I, KC_J, KC_K, KC_L
 ),
 
 /* GameFn
