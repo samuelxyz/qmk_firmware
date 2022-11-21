d113006d6b186a48860b9fc0c2d903db0ae45887
Samuel Tan <xinyizhentan@gmail.com>
2022-11-21 16:56:26 -0500

    add mods to gamefn

--- keyboards/lily58/keymaps/tanamr/keymap.c
+++ keyboards/lily58/keymaps/tanamr/keymap.c
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_GAME] = LAYOUT(
   KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y,    KC_U,    KC_UP,    KC_O,    KC_P,    KC_QUOT,
-  MO(_GAMEFN), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_LEFT, KC_DOWN, KC_RGHT, KC_QUOT,
+  MO(_GAMEFN), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_LEFT, KC_DOWN, KC_RGHT, KC_SCLN, KC_BSPC,
   KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_H, KC_F2, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
   KC_RCTL, KC_LSFT, KC_SPC, KC_ENT, KC_I, KC_J, KC_K, KC_L
 ),
@@ -98,10 +98,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | TRNS |  F6  |  F7  |  F8  |  F9  |  F10 |-------.    ,-------|      |      |      |      |      |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |  F11 |  F12 |      |      |      |-------|    |-------|      |      |      |      |      |      |
+ * |LCtrl |  F11 |  F12 |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      |      |      | /Rollla /       \      \  |      |      |      |
- *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   | LAlt | Shift| Shift| /Rollla /       \      \  |      |      |      |
+ *                   |(RCtrl)      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
@@ -109,8 +109,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
   KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
   KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-  KC_NO, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-  KC_NO, KC_NO, KC_NO, TO(_ROLLLA), KC_NO, KC_NO, KC_NO, KC_NO
+  KC_LCTL, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+  KC_RCTL, KC_LSFT, KC_LSFT, TO(_ROLLLA), KC_NO, KC_NO, KC_NO, KC_NO
 ),
 
 /* Symbols/Numpad
