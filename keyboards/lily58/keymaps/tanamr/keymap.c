faf2a002dda8950dd57cd5972c2275ab955d61a7
Samuel Tan <xinyizhentan@gmail.com>
2022-12-07 14:42:01 -0500

    tweak gamin layers

--- keyboards/lily58/keymaps/tanamr/keymap.c
+++ keyboards/lily58/keymaps/tanamr/keymap.c
@@ -73,11 +73,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |  Up  |   O  |   P  |  '   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |GameFn|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  | Left |  Dn  |Right |   ;  | Bksp |
+ * |GameFn|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  | Left |  Dn  |Right |   ;  |Enter |
  * |------+------+------+------+------+------|   H   |    |  F2   |------+------+------+------+------+------|
- * |LCtrl |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  -   |
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  -   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | Shift|Space | / Enter /       \   I  \  |   J  |   K  |   L  |
+ *                   | LAlt |LCtrl |Space | /BackSP /       \   I  \  |   J  |   K  |   L  |
  *                   |(RCtrl)      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -85,9 +85,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_GAME] = LAYOUT(
   KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y,    KC_U,    KC_UP,    KC_O,    KC_P,    KC_QUOT,
-  MO(_GAMEFN), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_LEFT, KC_DOWN, KC_RGHT, KC_SCLN, KC_BSPC,
-  KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_H, KC_F2, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-  KC_RCTL, KC_LSFT, KC_SPC, KC_ENT, KC_I, KC_J, KC_K, KC_L
+  MO(_GAMEFN), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_LEFT, KC_DOWN, KC_RGHT, KC_SCLN, KC_ENT,
+  KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_H, KC_F2, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+  KC_RCTL, KC_LCTL, KC_SPC, KC_BSPC, KC_I, KC_J, KC_K, KC_L
 ),
 
 /* GameFn
@@ -96,9 +96,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | TRNS |  F6  |  F7  |  F8  |  F9  |  F10 |-------.    ,-------|      |      |      |      |      |      |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |LCtrl |  F11 |  F12 |      |      |      |-------|    |-------|      |      |      |      |      |      |
+ * | TRNS |  F6  |  F7  |  F8  |  F9  |  F10 |-------.    ,-------| TRNS |      |      |      |      |      |
+ * |------+------+------+------+------+------| TRNS  |    |       |------+------+------+------+------+------|
+ * |LCtrl |  F11 |  F12 |      |      | TRNS |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | Shift| Shift| /Rollla /       \      \  |      |      |      |
  *                   |(RCtrl)      |      |/       /         \      \ |      |      |      |
@@ -109,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
   KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
   KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-  KC_LCTL, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+  KC_LCTL, KC_F11, KC_F12, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
   KC_RCTL, KC_LSFT, KC_LSFT, TO(_ROLLLA), KC_NO, KC_NO, KC_NO, KC_NO
 ),
 
