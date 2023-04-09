4b34a2ad47cc43a7ea4535998bcfe7241fc73e49
Samuel Tan <xinyizhentan@gmail.com>
2023-04-08 23:24:09 -0400

    initial rgb add

--- keyboards/lily58/keymaps/tanamr/rules.mk
+++ keyboards/lily58/keymaps/tanamr/rules.mk
@@ -4,12 +4,16 @@ EXTRAKEY_ENABLE = yes        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE = yes     # OLED display
 COMBO_ENABLE = yes
+LTO_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
 #SRC += #./lib/rgb_state_reader.c \
